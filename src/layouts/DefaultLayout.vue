37e92fd4ec0346850edf6da721a262b533f60b10
Javier Diaz <coderdiaz@gmail.com>
2018-01-26 18:05:55 -0600

    Preparing project for development

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="default-layout-app">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <default-layout>
+    <h1>Home View</h1>
+  </default-layout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/DefaultLayout'
+export default {
+  name: 'HomeView',
+  components: {
+    DefaultLayout
+  }
+}
+</script>
